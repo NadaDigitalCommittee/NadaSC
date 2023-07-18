a1e7e0521e56f4b46db16d67d30393a9c30e58dd
altusershared <104960044+altusershared@users.noreply.github.com>
2023-07-18 23:48:06 +0900

    生徒会長挨拶更新

--- pages/council/index.vue
+++ pages/council/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h1 class="midashi">生徒会長挨拶<span>Message from the President</span></h1>
+    <p class="lead">昨年度の無制限文化祭と熱気に包まれた体育祭。</p>
+    <p>
+      コロナ前を超える活気だったのは間違いない。ポストコロナ時代を生きる我々は、教訓を活かし、「より強靭な生徒会」を目指し、また受け継がれてきた灘校の魅力と伝統が途絶えないように、そして今よりさらに深みのあるものにするには何が必要なのか、常に模索し続けなければなりません。
+    </p>
+    <p>
+      創立100周年のカウントダウンへ向けて、発展の1年を共に築き上げていきましょう。
+    </p>
+    <h2 class="name"><span>第72代生徒会長</span>山岸侑生</h2>
+    <!--
+    <h1 class="midashi">生徒会長挨拶<span>Message from the President</span></h1>
     <p class="lead">2019年12月にはじまり、ここ数年世界で猛威を振るう新型コロナウイルス。</p>
     <p>
       灘校も世間と同じく、さまざまな影響を受けました。文化祭、体育祭は短縮開催を余儀なくされ、綿々と受け継がれてきた伝統も損なわれつつあります。
@@ -9,6 +19,7 @@
       でも、今更コロナでできなかったことを数えても何も生まれません。僕はこのコロナによる分断をチャンスだと捉えたい。一度リセットし、もう一度灘校に必要なことは何なのかを考え、魅力ある灘校を作り上げていきます。ここから始まる新たな1ページを期待してください。
     </p>
     <h2 class="name"><span>第71代生徒会長</span>髙島晟輔</h2>
+    -->
     <!--
     <p class="lead">変化の絶えない現代、灘校生徒会も変革を迫られています。</p>
     <p>
@@ -37,6 +48,7 @@ export default {
   margin-top: 2em;
   margin-bottom: 1.5em;
   font-size: 1.7em;
+
   span {
     display: block;
     font-size: 0.5em;
@@ -50,6 +62,7 @@ p {
   line-height: 1.5;
   text-align: justify;
 }
+
 p.lead::first-letter {
   font-size: 2em;
 }
@@ -66,14 +79,18 @@ h2 {
     font-size: 1.2rem;
   }
 }
+
 @media screen and (min-width: 960px) {
+
   p,
   h2 {
     margin: 0 30%;
   }
+
   p {
     margin-bottom: 0.5em;
   }
+
   h2 {
     margin-top: 1em;
   }
